abbae98997f6645070886334a85bf8d67794b88c
Dhruv Kashyap <dhruvkashyap10a22@gmail.com>
2026-08-20 20:33:20 +0530

    Time: 0 ms (100%), Space: 47.1 MB (99.3%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int trap(int[] arr) {
+        int n = arr.length;
+
+        int l = 0;
+        int r = n-1;
+        int leftMax=Integer.MIN_VALUE;
+        int rightMax = Integer.MIN_VALUE;
+        int water = 0;
+
+        while(l<r){
+            if(arr[l]<arr[r]){
+                leftMax = Math.max(leftMax, arr[l]);
+                water += leftMax - arr[l];
+                l++;
+            }else{
+                rightMax = Math.max(rightMax, arr[r]);
+                water += rightMax - arr[r];
+                r--;
+            }
+        }
+
+        return water;
+    }
+}
